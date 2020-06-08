bb2f661f8268af1d616ca422e70383def5cb9424
Gracie <m.graciemcguire@gmail.com>
2020-06-08 11:02:14 -0600

    adds router and skeleton components

---
+++ src/Components/Nav.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Nav = (props) => {
+  return(
+    <div>Your code here</div>
+  )
+}
+export default FuncName
---
+++ src/Pages/index.js
@@ -0,0 +1,9 @@
+import Signup from './Signup'
+import Login from './Login'
+import Home from './Home'
+
+export default {
+  Signup,
+  Login,
+  Home
+}
