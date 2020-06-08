b6cbd0ff6f9775f019da17acceeaf59f73c70d62
Gracie <m.graciemcguire@gmail.com>
2020-06-08 12:56:33 -0600

    nav

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { userActions } from '../redux/actions'
 
-const Nav = (props) => {
+const Nav = () => {
+
+  const dispatch = useDispatch()
+
+  const handleLogout = () => {
+    dispatch(userActions.logoutUser())
+  }
+
   return(
-    <div>Your code here</div>
+    <nav style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+      <Link to='/'> home </Link>
+      <Link to='/signup'> sign up </Link>
+      <Link to='/login'> login </Link>
+      <Link to='/' onClick={ handleLogout }> log out </Link>
+    </nav>
   )
 }
-export default FuncName
+export default Nav;
