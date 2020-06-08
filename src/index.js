dbe75d8e69d944251281b5f91ce5651a6ce108f5
Gracie <m.graciemcguire@gmail.com>
2020-06-08 11:37:58 -0600

    add user login and logout actions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { rootReducer } from './redux/rootReducer';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={ store }>
     <App />
-  </React.StrictMode>,
+  </Provider >,
   document.getElementById('root')
 );
 
---
+++ src/redux/actions.js
@@ -0,0 +1,102 @@
+// constant API URLS
+
+const BASE_URL = 'http://localhost:3000'
+const USERS_URL = BASE_URL + '/users'
+const PERSIST_URL = BASE_URL + '/persist'
+const LOGIN_URL = BASE_URL + '/login'
+const SPECIFIC_USER_URL = id = > USERS_URL + '/' + id
+
+
+//redux actions
+
+const setUserAction = userObj => ({
+  type: 'SET_USER',
+  payload: userObj
+})
+
+const clearUserAction = () => ({
+  type: 'CLEAR_USER'
+})
+
+
+//fetchies
+
+const newUserToDB = userObj => dispatch => {
+
+  const config = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(userObj)
+  };
+
+  fetch(USERS_URL, config)
+  .then(r => r.json())
+  .then(data => {
+    dispatch(setUserAction(data.user))
+    localStorage.setItem('token', data.token)
+  })
+}
+
+const deleteUserFromDB = userId => dispatch => {
+
+  const config = {
+    method: 'DELETE'
+  };
+
+  fetch(SPECIFIC_USER_URL(id), config)
+  .then(r => {
+    dispatch(clearUserAction())
+    localStorage.clear()
+  })
+}
+
+const loginUserToDB = userCredentials => dispatch => {
+
+  const config = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(userCredentials)
+  };
+
+  fetch(LOGIN_URL, config)
+  .then(r => r.json())
+  .then(data => {
+    dispatch(setUserAction(data.user))
+    localStorage.setItem('token', data.token)
+  });
+
+}
+
+const persistUser = () => dispatch => {
+
+  const config = {
+    method: 'GET',
+    headers: {
+      'Authorization': `bearer ` + localStorage.token
+    }
+  }
+
+  fetch(PERSIST_URL, config)
+  .then(r => r.json())
+  .then(userInstance => {
+    dispatch(setUserAction(userInstance))
+  })
+
+}
+
+const logoutUser = () => dispatch => {
+  dispatch(clearUserAction())
+  localStorage.clear()
+}
+
+export default {
+  newUserToDB,
+  deleteUserFromDB,
+  loginUserToDB,
+  persistUser,
+  logoutUser
+}
