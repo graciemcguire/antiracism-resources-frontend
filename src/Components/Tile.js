8941ad4f224557a306705aa2b7e52d3f9f017888
luanesouza <luanesouza411@gmail.com>
2020-06-13 15:53:15 -0400

    adds iframe

--- src/Components/Tile.js
+++ src/Components/Tile.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import ReactPlayer from "react-player"
+import YouTubePlayer from 'react-player/lib/players/YouTube';
 
 const Tile = ({resource}) => {
-  // const allResources = resources.map( resource => <Tile key={resource.id} resource={resource} />)
-
-  // console.log(resource);
+  console.log(resource);
+  const mediaRendering = (url) => {
+    if(url.includes('youtu.be')){
+      return (<ReactPlayer
+              url={`${url}`}
+              width={`70vw`}
+              height={'100%'}
+            />)
+    } else {
+      return (<a href={url}> Read More </a>)
+    }
+  }
 
   return(
     <section className='tile'>
       <h2>{resource.title}</h2>
       <p>{resource.description}</p>
-
+      <hr />
+      {mediaRendering(resource.url)}
     </section>
   )
 }
