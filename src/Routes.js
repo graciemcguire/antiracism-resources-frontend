219736e7a39e6799940e3f816246661935f2ebf2
luanesouza <luanesouza411@gmail.com>
2020-10-16 17:48:37 -0300

    renames index

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import Pages from './Pages'
+import Pages from './Pages/Page_index'
 
 const Routes = () => {
   return (
