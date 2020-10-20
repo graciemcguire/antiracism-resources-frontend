6295529cc9827b629672ec287a2575e298e4ec89
Gracie McGuire <40892567+graciemcguire@users.noreply.github.com>
2020-10-20 12:10:02 -0400

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
-// const BASE_URL = 'https://antiracism-api.herokuapp.com/'
-const BASE_URL = 'http://localhost:3000/'
+const BASE_URL = 'https://antiracism-api.herokuapp.com/'
+// const BASE_URL = 'http://localhost:3000/'
 
 const getAllResources = async () => {
   const response = await axios(`${BASE_URL}/resources`);
-  console.log('response.data in utils', response.data)
   return response.data;
 }
 
