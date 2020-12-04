bc81cb1f08634edcbd37fe164f039fcc9689fbe7
Gracie <m.graciemcguire@gmail.com>
2020-12-04 17:39:01 -0500

    about div

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,8 +5,8 @@ const About = () => {
   return(
     
     <section className='about'>
-      <h2> 'About' </h2>
-      <p>'lorem ipsum'</p>
+      <h1>About Us</h1>
+      <p>Anti-Racism Resources was created by Gracie McGuire and Annie Souza in 2020 as a response to the influx of resources being passed around on the depths and history of racism and the systems upholding and perpetuating it worldwide. We are actively trying to add more resources, and are more than welcome to submissions! </p>
     </section>
   )
 }
